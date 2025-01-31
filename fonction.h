1ed236b770dbc0c01397afabd5a6d782a7dfdc9a
ltqdf <91142943+ltqdf@users.noreply.github.com>
2025-01-31 10:49:25 +0100

    Update fonction.h

--- fonction.h
+++ fonction.h
@@ -20,6 +20,7 @@ struct Tensions {
     float cabine;
     float contrepoids;
 };
+
 /*
 {
     R : la fonction permet de calculer les tension des cable c'est a dire T_cabine et T_contrepoids
@@ -28,7 +29,7 @@ struct Tensions {
     S :ils renvoie soit l'une des deux tension soit les deux en fonction du choix de l'utulisateur grace a un struct 
 }
 */
-Tensions CalculeTension(int choix, float masse_cabine = 0 ,float masse_contrepoids = 0  ,float alpha = 0);
+Tensions CalculeTension(int choix, float masse_cabine = 0, float masse_contrepoids = 0, float alpha = 0);
 
 /*
 {
@@ -37,7 +38,7 @@ Tensions CalculeTension(int choix, float masse_cabine = 0 ,float masse_contrepoi
     S :
 }
 */
-float puissance_moteur(float couple_mot = 0, float vitesse = 0, float rayon = 0);
+float couple_moteur(int choix, float tension_cabine = 0, float tension_contrepoids = 0, float alpha = 0, float moments_dinertie = 0, float rayon = 0, float puissance_moteur = 0, float vitesse = 0);
 
 /*
 {
@@ -46,7 +47,55 @@ float puissance_moteur(float couple_mot = 0, float vitesse = 0, float rayon = 0)
     S :
 }
 */
-float rayon_poulie(float vitesse = 0, float  vitesse_angulaire = 0);
+float puissance_moteur(float couple_mot = 0, float vitesse = 0, float rayon = 1);
 
 
 
+/*
+{
+    R :
+    E :
+    S :
+}
+*/
+float rayon_poulie(float vitesse = 0, float  vitesse_angulaire = 1);
+
+
+/*
+{
+    R :
+    E :
+    S :
+}
+*/
+float vitesse_rotation(float vitesse_angulaire = 1);
+
+
+/*
+{
+    R :
+    E :
+    S :
+}
+*/
+float temps_montee_and_descente(float distance = 1, float vitesse = 0);
+
+
+/*
+{
+    R :
+    E :
+    S :
+}
+*/
+float acceleration(int choix = 0, float masse_contrepoids = 0, float masse_cabine = 0, float tension_contrepoids = 0, float tension_cabine = 0, float vitesse = 0);
+
+
+/*
+{
+    R :
+    E :
+    S :
+}
+*/
+void test_fonction();
