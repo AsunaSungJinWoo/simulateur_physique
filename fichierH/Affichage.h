36f160d61750ddbb9d29d8f8dfec39d109f4841f
théo <91142943+AsunaSungJinWoo@users.noreply.github.com>
2025-03-13 10:48:35 +0100

    Create Affichage.h

---
+++ fichierH/Affichage.h
@@ -0,0 +1,17 @@
+#pragma once
+
+/*
+    Fonction : Affichage
+
+    Entrée :
+        - Aucune
+
+    Rôle :
+        - Affiche le menu principal contenant les différentes options disponibles pour l'utilisateur.
+        - Gère la saisie utilisateur et oriente vers les calculs ou actions correspondantes.
+        - Vérifie la validité de la saisie pour éviter les erreurs.
+
+    Retour :
+        - Aucune valeur de retour (void).
+*/
+void Affichage(void);
