2fb94e0842731b94d9d3b5e96b553200f89feac9
théo <91142943+AsunaSungJinWoo@users.noreply.github.com>
2025-03-14 22:32:49 +0100

    Update Affichage.h

--- fichierH/Affichage.h
+++ fichierH/Affichage.h
@@ -1,16 +1,18 @@
 #pragma once
 
-
 /*
-
-
-
+    E: prend en valeur une variable qui sera modifiée en même temps que "valeur" car il y a int&.
+    R: la fonction i_SecuriserSaisie permet dans un premier temps de demander à l'utilisateur une donnée précise. Dans ce cas, on veut un entier et tant que les données
+        d'entrée ne sont pas conformes, la fonction redemande à l'utilisateur la valeur.
+    S: la sortie est en void mais elle affecte bien la valeur d'entrée, donc elle renvoie quand même quelque chose.
 */
 void i_SecuriserSaisie(int& valeur);
 
 /*
-
-
+    E: prend en valeur une variable qui sera modifiée en même temps que "valeur" car il y a float&.
+    R: la fonction f_SecuriserSaisie permet dans un premier temps de demander à l'utilisateur une donnée précise. Dans ce cas, on veut un float et tant que les données
+        d'entrée ne sont pas conformes, la fonction redemande à l'utilisateur la valeur.
+    S: la sortie est en void mais elle affecte bien la valeur d'entrée, donc elle renvoie quand même quelque chose.
 */
 void f_SecuriserSaisie(float& valeur);
 
